83f7c45bf157fd897e61ebfc22cce6798d5baab1
Claudio Pereira <pereirclaudio501@gmail.com>
2026-02-12 15:38:13 -0300

    aula12

---
+++ Young2/aula12/script.js
@@ -0,0 +1,21 @@
+document.body.innerHTML = "<h1>Hello World!</h1>"
+
+//Comentarios de uma linha são utilizados duas barras(//)
+
+/* Comentario de multiplas linhas
+São utilizados "/* */
+
+let nome = 'Claudio'
+let cidade = 'itupeva'
+let cep, bairro
+
+cep = 13299.136
+bairro = 'Nova Monte Serrat'
+
+const ddd = 55
+
+let teste = false
+
+console.log(`Olá ${nome}, você mora na cidade de ${cidade} com cep: ${cep} - Bairro: ${bairro} e seu DDD ${ddd}`)
+
+document.body.innerHTML = ` ${typeof(nome)}, ${typeof(cep)}, ${typeof(ddd)}, ${typeof(teste)} `
