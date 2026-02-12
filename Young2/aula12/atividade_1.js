01f90c46ed04e8f110a86814d006adb3c62ea955
Claudio Pereira <pereirclaudio501@gmail.com>
2026-02-12 15:56:18 -0300

    aula12

---
+++ Young2/aula12/atividade_1.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById("btn")
+
+function inserir_texto(){
+    if (btn.innerText == "Aperte o botão"){
+        btn.innerText = "Aperteu o botão"
+    }else{
+        btn.innerText = "Aperte o botão"
+    }
+}
